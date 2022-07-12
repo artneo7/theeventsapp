ebd23c17ca31d6341b8a279cdcb0095a862e7e62
artneo7 <contato@artneo.com.br>
2022-07-12 15:49:57 -0300

    event date and type

--- src/components/Event.svelte
+++ src/components/Event.svelte
@@ -19,6 +19,14 @@
   {/if}
 
   <h2>{event.title}</h2>
+
+  <span class="event__date">{event.date.slice(0, 5)}</span>
+
+  <div class="event__type" class:blue="{event.category === 'Birthday'}" class:red="{event.category === 'Relationship'}" class:green="{event.category === 'Important'}">
+    <span class="line-1"></span>
+    <span class="line-2"></span>
+    <span class="line-3"></span>
+  </div>
 </div>
 
 <style>
@@ -61,5 +69,38 @@
     max-width: 40px;
     border-radius: 50%;
     box-shadow: 0 0 0 2px #fff, 0 0 0 4px currentColor;
+  }
+  .event__date {
+    color: var(--g5);
+    background-color: #fff;
+    padding: 0px 12px;
+    border-radius: 16px;
+    margin-left: auto;
+    margin-right: 20px;
+    z-index: 1;
+  }
+  .event__type {
+    transform: rotate(-45deg);
+    position: absolute;
+    top: 0;
+    right: -80px;
+  }
+  .event__type .line-1 {
+    display: block;
+    width: 240px;
+    height: 8px;
+    background-color: currentColor;
+  }
+  .event__type .line-2 {
+    display: block;
+    width: 240px;
+    height: 4px;
+    background-color: transparent;
+  }
+  .event__type .line-3 {
+    display: block;
+    width: 240px;
+    height: 16px;
+    background-color: currentColor;
   }
 </style>
